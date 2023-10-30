9941bfc4cf42e3860ce6f89f494eb24cfa87919f
Sergio Pinto <serrique@icloud.com>
2023-10-30 16:18:35 -0400

    Partial Delivery by task 5
    Signed-off-by: Sergio Pinto <serrique@icloud.com>

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,9 +6,19 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!isValid(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});
+  });
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
@@ -26,19 +36,45 @@ public_users.get('/isbn/:isbn',function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+
+    const author = req.params.author;
+    const matchingBooks = [];
+    for (const isbn in books) {
+        const book = books[isbn];
+        if (book.author === author) {
+          matchingBooks.push(book);
+        }
+      }
+    res.json(matchingBooks)
+
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.title;
+  const matchingBooks = [];
+  for (const isbn in books) {
+      const book = books[isbn];
+      if (book.title === title) {
+        matchingBooks.push(book);
+      }
+    }
+  res.json(matchingBooks)
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const review = req.params.isbn;
+  const matchingBooks = [];
+  for (const isbn in books) {
+      const book = books[isbn];
+      if (isbn === review) {
+        matchingBooks.push(book.reviews);
+      }
+    }
+  res.json(matchingBooks)
 });
 
 module.exports.general = public_users;
